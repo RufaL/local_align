0401dcf93b3203d5d64e8aebcb9ed639a6f35e70
RufaL <63213924+RufaL@users.noreply.github.com>
2020-04-16 21:16:59 -0400

    First commit

---
+++ swalign.h
@@ -0,0 +1,40 @@
+/*
+ * Header file for SW algorithm functions: scoring matrix computation, 
+ * DP matrix recurrence function implementation, rules for scoring like
+ * score for match/mismatch and affine-gap penalty score, data structure of 
+ * entries in SW scoring matrix
+ */
+#ifndef __SWALGIN_H__
+#define __SWALGIN_H__
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "stdint.h"
+
+/*Enumeration of DP matrices*/
+enum DP_dir{
+ 	m,            //Value 0 for diagonal direction :match/mismatch
+ 	x,            //Value 1 for x-direction : gap in sequence x
+ 	y             //Value 2 for y-direction : gap in sequence y
+};
+
+
+/* Scoring Matrix entry data structure*/
+typedef struct {
+	int value;    //Value of SW matrix entry
+    DP_dir direction;  //Direction: M, X or Y which giving maximum score
+}sw_entry;
+
+const int match = 10, mismatch = -2;      //Match and Mismatch scores
+const int gap_open = -15, gap_extn = -7; //Gap opening and extension penalty, values example
+
+int match_score(int i, int j, char *seq1, char *seq2); 
+
+void init_DP(int seq1_len, int seq2_len);
+
+sw_entry compute_DP(int seq1_i, int seq2_i, char *seq1, char *seq2);
+
+void traceback(sw_entry SW[][301], int seq1_len, int seq2_len, char *seq1, char *seq2, char *seq1_out, char *seq2_out);
+
+#endif
+
